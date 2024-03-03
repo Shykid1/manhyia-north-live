ec453abdb23301508eb022306e09652d7973c7ea
pious <130520977+codedemond@users.noreply.github.com>
2024-03-03 15:11:55 +0000

    updates

--- client/src/views/Home/DashContent.jsx
+++ client/src/views/Home/DashContent.jsx
@@ -129,7 +129,7 @@ function DashboardContent() {
           </Card>
         </div>
 
-        <Card sx={{ width: { xs: "100%", sm: `70%` } }}>
+        <Card sx={{ width: { xs: '80%', sm: `70%` } }}>
           <CardContent>
             <div className="flex-container column gap-10">
               <div className="flex-container gap-10 mapsdisplay">
