b24b4145a1d2fa851a1ad1bf9a7ccfaeeb18fd35
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-03-06 03:20:43 +0000

    Add login functionality to Login component

--- client/src/views/Auth/Login.jsx
+++ client/src/views/Auth/Login.jsx
@@ -1,38 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { FaUser } from "react-icons/fa";
-import {Button, Form} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/auth/login",
+        { email, password }
+      );
+      const token = response.data.token;
+      setEmail("");
+      setPassword("");
+      navigate("/dashboard");
+      window.location.reload();
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
   return (
     <div className="flex-container width-100 center logincontainer">
-
       <div className="loginform column gap-10 center flex-container">
-        <FaUser className="loginlogo"/>
+        <FaUser className="loginlogo" />
         <h5>LOGINS CREDENTIALS </h5>
-        <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We ll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We ll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Remember me" />
-      </Form.Group>
-      <Button variant="primary" type="submit" className="submitbt">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="submitbt">
+            Submit
+          </Button>
+        </Form>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
